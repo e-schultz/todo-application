63f2f55b3ae679d42cb53ab5d59b2cab6509372e
Tarek-123 <t.hasab@icloud.com>
2021-05-24 09:06:09 -0400

    create loader component

---
+++ src/components/UI/Loading/Loading.js
@@ -0,0 +1,13 @@
+import Loader from "react-loader-spinner";
+
+export default function Loading() {
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
+}
