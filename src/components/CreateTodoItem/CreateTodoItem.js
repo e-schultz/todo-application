5feb6519e9f9a1fc436ae64066d376f9603db8d8
Tarek-123 <t.hasab@icloud.com>
2021-05-24 08:57:51 -0400

    create todo item component

---
+++ src/components/CreateTodoItem/CreateTodoItem.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useState } from "react";
+import { MdAdd } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { sendTodo } from "../../store/actions/createTodo";
+
+import classes from "./CreateTodoItem.module.css";
+
+export default function CreateTodoItem() {
+  const [value, setValue] = useState();
+  const dispatch = useDispatch();
+
+  const addNewTodo = (event) => {
+    event.preventDefault();
+    dispatch(sendTodo(value));
+    setValue("");
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.items}>
+        <input
+          onChange={(e) => setValue(e.target.value)}
+          className={classes.input}
+          type="text"
+          value={value}
+        />
+      </div>
+      <button
+        onClick={(event) => addNewTodo(event)}
+        className={!value ? `${classes.btnDisabled}` : `${classes.btn}`}
+        disabled={!value}
+      >
+        <MdAdd className={classes.btnIcon} />
+      </button>
+    </Fragment>
+  );
+}
